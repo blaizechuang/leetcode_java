7d1f6e26e40976390784c471e1121608ae242b24
blaize.chuang <blaize.chuang@idrip.coffee>
2021-04-22 11:41:20 +0800

    Add select sort

--- app/src/main/java/com/leetcode/leetcodesolution/datastructure/select_sort.java
+++ app/src/main/java/com/leetcode/leetcodesolution/datastructure/select_sort.java
@@ -1,4 +1,38 @@
 package com.leetcode.leetcodesolution.datastructure;
 
-public class select_sort {
+import android.util.Log;
+
+import com.leetcode.leetcodesolution.solution.base_solution;
+
+public class select_sort implements base_solution {
+    @Override
+    public void execute() {
+        int[] num = {3,5,1,4};
+        int[] result = sort(num);
+        String str = "";
+        for (int i = 0; i < result.length; i++) {
+            str += result[i] + ", ";
+
+        }
+        Log.d("tag", "After sort: " + str);
+    }
+
+    private int[] sort(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int min = i;
+            for (int j = i+1; j < list.length; j++) {
+                if (list[min] > list[j]) {
+                    min = j;
+                }
+            }
+
+            if (min != i) {
+                int tmp = list[min];
+                list[min] = list[i];
+                list[i] = tmp;
+            }
+        }
+
+        return list;
+    }
 }
