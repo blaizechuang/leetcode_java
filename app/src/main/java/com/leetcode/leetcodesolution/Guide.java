37dc5bcf1daad5bb5e4ab278de927ab5b7f35fe9
blaize.chuang <blaize.chuang@idrip.coffee>
2021-06-22 19:19:00 +0800

    Add readme

---
+++ app/src/main/java/com/leetcode/leetcodesolution/Guide.java
@@ -0,0 +1,46 @@
+package com.leetcode.leetcodesolution;
+
+public class Guide {
+    /**
+     * Easy (9)
+     */
+    // 1. Two Sum
+    // 13. Roman to Integer
+    // 20. Valid Parentheses
+    // 21. Merge Two Sorted Lists
+    // 94. Binary Tree Inorder Traversal
+    // 100. Same Tree
+    // 101. Symmetric Tree
+    // 206. Reverse Linked List
+    // 226. Invert Binary Tree
+
+    /**
+     * Medium (12)
+     */
+    // 2. Add Two Numbers
+    // 8. String to Integer (atoi)
+    // 11. Container With Most Water
+    // 91. Decode Ways
+    // 95. Unique Binary Search Trees II
+    // 96. Unique Binary Search Trees
+    // 98. Validate Binary Search Tree
+    // 102. Binary Tree Level Order Traversal
+    // 103. Binary Tree Zigzag Level Order Traversal
+    // 107. Binary Tree Level Order Traversal II
+    // 151. Reverse Words in a String
+    // 904. Fruit Into Baskets
+
+    /**
+     * Hard (4)
+     */
+    // 4. Median of Two Sorted Arrays
+    // 10. Regular Expression Matching
+    // 23. Merge k Sorted Lists
+    // 42. Trapping Rain Water
+
+
+    /**
+     * LintCode (1)
+     */
+    // 1872. Minimum Cost to Connect Sticks
+}
