60904706c2d553f2c1e538c3ffd3ef503da7a1d3
blaize.chuang <blaize.chuang@idrip.coffee>
2021-07-20 16:44:37 +0800

    submit solution

--- app/src/main/java/com/leetcode/leetcodesolution/solution/google/leetcodepremium/easy/Valid_Parentheses_20.java
+++ app/src/main/java/com/leetcode/leetcodesolution/solution/google/leetcodepremium/easy/Valid_Parentheses_20.java
@@ -1,4 +1,4 @@
-package com.leetcode.leetcodesolution.solution.microsoft;
+package com.leetcode.leetcodesolution.solution.google.leetcodepremium.easy;
 
 import android.util.Log;
 
@@ -15,6 +15,10 @@ public class Valid_Parentheses_20 extends logger implements base_solution {
         Log.d("--", "isValid: " + isValid );
     }
 
+    /**
+     * runtime: 1 ms, 思路, 因為只需要處理 (){}[], 所以不管什麼就是 push 進去, 然後只有在看到 )]} 時
+     * 去判斷 stack 裡面的 top 是不是他們的對應字元, 是的話就 pop
+     */
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack();
         for (int i = 0; i < s.length(); i++)
