c8905acdb3245c6754cec178bab4ed263a7db243
blaize.chuang <blaize.chuang@idrip.coffee>
2021-07-26 21:38:44 +0800

    Submit solution

--- app/src/main/java/com/leetcode/leetcodesolution/solution/microsoft/Merge_two_Sorted_List_21.java
+++ app/src/main/java/com/leetcode/leetcodesolution/solution/microsoft/Merge_two_Sorted_List_21.java
@@ -10,6 +10,43 @@ public class Merge_two_Sorted_List_21 extends logger implements base_solution {
         //
     }
 
+    /**
+     * 相較於 recursive, 我還是比較喜歡 iteratively (迭代)
+     * 感覺比較直覺, 寫起來也順手
+     * Time complexity O(m+n)
+     * Space complexity O(1)
+     */
+    public ListNode mergeTwoLists_iteratively(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        if (l1 != null) {
+            dummy.next = l1;
+        }
+
+        if (l2 != null) {
+            dummy.next = l2;
+        }
+
+        return head.next;
+    }
+
+    /**
+     * Recursive, 我不愛, 而且容易 stackoverflow
+     */
     public ListNode mergeTwoLists_resur(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
