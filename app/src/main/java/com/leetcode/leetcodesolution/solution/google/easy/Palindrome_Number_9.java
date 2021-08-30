552cc3ebf55dc57dec721360c98bee88071ffcc7
blaize.chuang <blaize.chuang@idrip.coffee>
2021-08-30 10:13:51 +0800

    Submit solution

--- app/src/main/java/com/leetcode/leetcodesolution/solution/google/easy/Palindrome_Number_9.java
+++ app/src/main/java/com/leetcode/leetcodesolution/solution/google/easy/Palindrome_Number_9.java
@@ -3,22 +3,24 @@ package com.leetcode.leetcodesolution.solution.google.easy;
 class Palindrome_Number_9 {
 
     /**
-     * Leetcode 神人的解法, 這公式也太厲害, 不過 run time 也是需要 12 ms
-     * time complexity: O(N)
+     * Leetcode 的解法, 直接把數值切一半, 譬如 1221 變成 12, 21
+     * time complexity: O(log10(底線)N)
      * space complexity: O(1)
+     * run time: 6 ms
      */
     public boolean isPalindrome(int x) {
-        int result = 0;
-        int inputX = x;
-        while (x > 0) {
-            result = result * 10 + (x % 10);
-            x = x / 10;
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
         }
-        return result == inputX;
+        return (x==rev || x==rev/10);
     }
 
     /**
      * 下面是我自己想的先轉成 char[] 在用 sliding window 去解, 不過 run time 需要 14 ms
+     * 而且題目說不能轉成 String Orz...沒注意到, 這個解法不行
      * toCharArray 需要 O(N), sliding windows 只需要一半的時間 O(LogN)
      * time complexity: O(N)
      * space complexity: O(N)
